7b8c680ff72b657eb7472baa7c03b16924ab6daf
mohit kushwaha <40515852+mohit-codes@users.noreply.github.com>
2021-05-01 19:55:32 +0530

    Implemented Login | authentication functionality (#26)
    * dashboard screen added | clipper dependecy added
    
    * google fonts dependency added
    
    * google font asset added | dashboard screen update
    
    * dashboard updates | search field added
    
    * app title above text field added | commit
    
    * gradient background update
    
    * comment remove
    
    * assets added | login screen added | yaml updated
    
    * pin_code_fields dependecy added | fields added
    
    * login button added
    
    * commit
    
    * animation from email field to otp pin fileds | state management througgh setstate()
    
    * sync
    
    * changed condition state rendering from text to button
    
    * mock event calls
    
    * unit test added | Mocking Rest API
    
    * email validation dependency | few updates
    
    * Snack bar added
    
    * auththentication done | login done | route to dashboard after succes login done
    
    * removed unused import
    
    Co-authored-by: Ashutosh Sahu <53266261+Ashuto7h@users.noreply.github.com>

---
+++ client/lib/resources/api_provider.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:client/dashboardScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show Client;
+
+class Apiprovider {
+  Client client = Client();
+  auth(BuildContext context, String email, {String otp = ''}) async {
+    var url = Uri.parse("https://nutrihelpb.herokuapp.com/auth");
+
+    var jsonBody = otp == ''
+        ? jsonEncode({
+            'email': email,
+          })
+        : jsonEncode({
+            'email': email,
+            'otp': otp,
+          });
+    print(jsonBody);
+    // var decodedBody = jsonDecode(jsonBody);
+    // print(decodedBody['token']);
+
+    final res = await client.post(
+      url,
+      body: jsonBody,
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (res.statusCode == 200) {
+      var data = jsonDecode(res.body);
+      String status = data['msg'];
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(status)));
+      if (otp != '' && data['ok'] == true) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DashBoardScreen(),
+          ),
+        );
+      }
+    }
+  }
+}
