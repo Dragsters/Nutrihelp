758bbf76214791bd8b0a88b2cfd946661da2f3c4
Arqam <arqam@Arqams-MacBook-Air.local>
2021-06-25 00:21:55 +0530

    Added msg info and error handling

--- client/lib/add_patient_screen.dart
+++ client/lib/add_patient_screen.dart
@@ -1,5 +1,4 @@
 import 'package:client/resources/api_provider.dart';
-import 'package:client/resources/helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
